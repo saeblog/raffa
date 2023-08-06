7b13f0ebd721fb91a3a64a9a28d21956e547eef4
yogisbg <yogisbg@gmail.com>
2023-08-06 16:09:22 +0700

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -43,7 +43,7 @@ export default function ContactForm() {
     },
   });
 
- return (
+return (
     
   <div className="bg-gray-50 dark:bg-neutral-900" id="contact">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center">
@@ -54,7 +54,7 @@ export default function ContactForm() {
         </p>
       </div>
 
-     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
+   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
         <div>
           <h2 className="text-lg font-bold">Contact Raffa Parabola</h2>
           <p className="max-w-sm mt-4 mb-4 dark:text-neutral-400">
@@ -89,120 +89,120 @@ export default function ContactForm() {
 				  </div>
         </div>
 
+        <div>
+          {!isSubmitSuccessful && (
 
-    <div>
-      {!isSubmitSuccessful && (
+           <form onSubmit={handleSubmit(onSubmit)} className="my-10">
+                <input
+                type="checkbox"
+                id=""
+                className="hidden"
+                style={{ display: "none" }}
+                {...register("botcheck")}></input>
 
-       <form onSubmit={handleSubmit(onSubmit)} className="my-10">
-        <input
-          type="checkbox"
-          id=""
-          className="hidden"
-          style={{ display: "none" }}
-          {...register("botcheck")}></input>
+               <div className="mb-5">
+                 <input
+                    type="text"
+                    placeholder="Full Name"
+                    autoComplete="false"
+                    className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+                    errors.name
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                    }`}
+                    {...register("name", {
+                     required: "Full name is required",
+                     maxLength: 80,
+                    })}
+                 />
+                  {errors.name && (
+                  <div className="mt-1 text-red-600">
+                   <small>{errors.name.message}</small>
+                  </div>
+                  )}
+              </div>
 
-        <div className="mb-5">
-          <input
-            type="text"
-            placeholder="Full Name"
-            autoComplete="false"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-              errors.name
+              <div className="mb-5">
+                <label htmlFor="email_address" className="sr-only">
+                    Email Address
+                </label>
+                <input
+                  id="email_address"
+                  type="email"
+                  placeholder="Email Address"
+                  name="email"
+                  autoComplete="false"
+                  className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+                  errors.email
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                  }`}
+                  {...register("email", {
+                  required: "Enter your email",
+                  pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Please enter a valid email",
+                  },
+                  })}
+                />
+                {errors.email && (
+                <div className="mt-1 text-red-600">
+                  <small>{errors.email.message}</small>
+                </div>
+                )}
+            </div>
+
+            <div className="mb-3">
+              <textarea
+                name="message"
+                placeholder="Your Message"
+                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
+                errors.message
                 ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
                 : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-            }`}
-            {...register("name", {
-              required: "Full name is required",
-              maxLength: 80,
-            })}
-          />
-          {errors.name && (
-            <div className="mt-1 text-red-600">
-              <small>{errors.name.message}</small>
+               }`}
+               {...register("message", {
+                required: "Enter your Message",
+               })}
+              />
+              {errors.message && (
+              <div className="mt-1 text-red-600">
+                {" "}
+               <small>{errors.message.message}</small>
+              </div>
+              )}
             </div>
-          )}
-        </div>
 
-        <div className="mb-5">
-          <label htmlFor="email_address" className="sr-only">
-            Email Address
-          </label>
-          <input
-            id="email_address"
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            autoComplete="false"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-              errors.email
-                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-            }`}
-            {...register("email", {
-              required: "Enter your email",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "Please enter a valid email",
-              },
-            })}
-          />
-          {errors.email && (
-            <div className="mt-1 text-red-600">
-              <small>{errors.email.message}</small>
-            </div>
-          )}
-        </div>
-
-        <div className="mb-3">
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
-              errors.message
-                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-            }`}
-            {...register("message", {
-              required: "Enter your Message",
-            })}
-          />
-          {errors.message && (
-            <div className="mt-1 text-red-600">
-              {" "}
-              <small>{errors.message.message}</small>
-            </div>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
-          {isSubmitting ? (
-            <svg
-              className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24">
-              <circle
+            <button
+              type="submit"
+              className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
+              {isSubmitting ? (
+              <svg
+                className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24">
+               <circle
                 className="opacity-25"
                 cx="12"
                 cy="12"
                 r="10"
                 stroke="currentColor"
                 strokeWidth="4"></circle>
-              <path
+               <path
                 className="opacity-75"
                 fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          ) : (
-            "Send Message"
-          )}
-        </button>
-       </form>
-      )}
+                </svg>
+                ) : (
+                "Send Message"
+               )}
+            </button>
+
+           </form>
+         )}
        
-       {isSubmitSuccessful && isSuccess && (
+          {isSubmitSuccessful && isSuccess && (
             <>
               <div className="flex flex-col items-center justify-center text-center text-white rounded-md">
                 <svg
@@ -266,11 +266,9 @@ export default function ContactForm() {
               </button>
             </div>
           )}   
-
-    </div>
-
+       </div>
 
     </div> 
   </div>
-  );
+);
 }
